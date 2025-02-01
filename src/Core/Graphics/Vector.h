15ba362b94cf9fc08143586f2b896757b97597fb
twje <11610634+twje@users.noreply.github.com>
2025-02-01 14:39:31 +1100

    Implement rotating 3D cube

--- src/Core/Graphics/Vector.h
+++ src/Core/Graphics/Vector.h
@@ -27,6 +27,40 @@ public:
 };
 
 //------------------------------------------------------------------------------
+template<typename T>
+class Vector3
+{
+public:
+	Vector3()
+		: x(static_cast<T>(0))
+		, y(static_cast<T>(0))
+		, z(static_cast<T>(0))
+	{ }
+
+	Vector3(T x, T y, T z)
+		: x(x)
+		, y(y)
+		, z(z)
+	{ }
+
+	// Template constructor to allow implicit conversion from another Vector2 type
+	template<typename U>
+	explicit Vector3(const Vector3<U>& other)
+		: x(static_cast<T>(other.x))
+		, y(static_cast<T>(other.y))
+		, z(static_cast<T>(other.z))
+	{ }
+
+	T x;
+	T y;
+	T z;
+};
+
+//------------------------------------------------------------------------------
 using Vector2f = Vector2<float>;
 using Vector2u = Vector2<uint32_t>;
 using Vector2i = Vector2<int32_t>;
+
+using Vector3f = Vector3<float>;
+using Vector3u = Vector3<uint32_t>;
+using Vector3i = Vector3<int32_t>;
--- src/Main.cpp
+++ src/Main.cpp
@@ -28,7 +28,7 @@ public:
     {
 		const Vector2i windowSize = mContext.GetWindowSize();
 
-        if (x < windowSize.x && y < windowSize.y)
+        if (x > 0 && x < windowSize.x && y > 0 && y < windowSize.y)
         {
             mBuffer[y * windowSize.x + x] = color;
         }
@@ -91,6 +91,34 @@ private:
 };
 
 //------------------------------------------------------------------------------
+class Camera
+{
+public:
+    Camera()
+        : Camera({ 0, 0 }, { 0.0f, 0.0f }, 90.0f)
+	{ }
+
+    Camera(Vector2i position, Vector2f rotation, float fov)
+		: mPosition(position)
+		, mRotation(rotation)
+		, mFov(fov)
+	{ }
+
+	void SetPosition(const Vector2i& position) { mPosition = position; }
+	void SetRotation(const Vector2f& rotation) { mRotation = rotation; }
+	void SetFov(float fov) { mFov = fov; }
+
+	const Vector2i& GetPosition() const { return mPosition; }
+	const Vector2f& GetRotation() const { return mRotation; }
+	float GetFov() const { return mFov; }
+
+private:
+	Vector2i mPosition;
+	Vector2f mRotation;
+	float mFov;
+};
+
+//------------------------------------------------------------------------------
 class RendererApplication : public Application
 {
 public:
@@ -101,6 +129,30 @@ public:
     virtual void OnCreate() override
     {
 		mPixelRenderer = std::make_unique<PixelRenderer>(GetContext());
+
+        // From -1 to 1 (in 9x9x9 cube)
+        mCubePoints.reserve(9 * 9 * 9);
+        mProjectedPoints.resize(9 * 9 * 9);		
+
+        auto projectPoint = [](const Vector3f& point) -> Vector2f
+        {
+            const float fov = 640.0f;
+            float newX = (point.x * fov) / point.z;
+            float newY = (point.y * fov) / point.z;
+            return Vector2f{ newX, newY };
+        };
+        
+
+		for (float x = -1; x <= 1.0f; x += 0.25f)
+		{
+            for (float y = -1; y <= 1.0f; y += 0.25f)
+            {
+                for (float z = -1; z <= 1.0f; z += 0.25f)
+                {					
+                    mCubePoints.push_back({ x, y, z });
+                }
+            }
+		}
     }
 
     virtual void OnEvent(const SDL_Event& event) override
@@ -110,17 +162,94 @@ public:
 
     virtual void OnUpdate() override
     { 
+        Vector3f cameraPosition { 0.0f, 0.0f, -5.0f };
         
+        mCubeRotation.x += 0.01f;
+        mCubeRotation.y += 0.01f;
+        mCubeRotation.z += 0.01f;
+
+        for (size_t i = 0; i < mCubePoints.size(); i++)
+        {
+			Vector3f point = mCubePoints[i];
+
+            Vector3f transformedPoint = RotateAboutX(point, mCubeRotation.x);
+            transformedPoint = RotateAboutY(transformedPoint, mCubeRotation.y);
+            transformedPoint = RotateAboutZ(transformedPoint, mCubeRotation.z);
+        
+            // Translate the points away from the camera
+            transformedPoint.z -= cameraPosition.z;
+
+            // Project the current point
+            Vector2f projected_point = Project(transformedPoint);
+
+            // Save the projected 2D vector in the array of projected points
+            mProjectedPoints[i] = projected_point;
+        }
     }
 
     virtual void OnRender() override
     {
         mPixelRenderer->Clear(0x00000000);
-        DrawRectangle(300, 200, 300, 150, 0xFFFF00FF);
+        
+		const Vector2i windowSize = GetContext().GetWindowSize();
+
+		for (const Vector2f& projectedPoint : mProjectedPoints)
+		{
+            DrawRectangle(
+                static_cast<int32_t>(projectedPoint.x + (windowSize.x * 0.5f)),
+                static_cast<int32_t>(projectedPoint.y + (windowSize.y * 0.5f)),
+                4,
+                4,
+                0xFFFFFFFF
+            );
+		}
+
 		mPixelRenderer->Render();
     }
 
 private:
+	Vector3f RotateAboutX(const Vector3f& point, float angle)
+	{
+		const float s = sin(angle);
+		const float c = cos(angle);
+
+		return {
+			point.x,
+			point.y * c - point.z * s,
+			point.y * s + point.z * c,
+		};
+	}
+
+    Vector3f RotateAboutY(const Vector3f& point, float angle)
+    {
+        const float s = sin(angle);
+        const float c = cos(angle);
+
+        return {
+            point.x * c - point.z * s,
+            point.y,
+            point.x * s + point.z * c,
+        };
+    }
+
+	Vector3f RotateAboutZ(const Vector3f& point, float angle)
+	{
+		const float s = sin(angle);
+		const float c = cos(angle);
+
+        return {
+			point.x * c - point.y * s,
+			point.x * s + point.y * c,
+			point.z
+		};
+    }
+
+	Vector2f Project(const Vector3f& point)
+	{
+		const float fov = 640.0f;		
+        return { (point.x * fov) / point.z, (point.y * fov) / point.z };
+	}
+
 	void DrawRectangle(int32_t x, int32_t y, int32_t width, int32_t height, int32_t color)
 	{
         for (int32_t i = 0; i <= width; i += 1)
@@ -178,6 +307,9 @@ private:
     }
 
     std::unique_ptr<PixelRenderer> mPixelRenderer;
+	std::vector<Vector3f> mCubePoints;
+    std::vector<Vector2f> mProjectedPoints;    
+    Vector3f mCubeRotation;
 };
 
 //------------------------------------------------------------------------------
